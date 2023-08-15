6eaa1ef5f6c445afc7724983e0655f10f997a97f
manniu1542 <819400842@qq.com>
2023-08-16 07:50:11 +0800

    UGUI 研究  项目构建

---
+++ Assets/Scripts/Tets.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UI;
+using UnityEngine.UI;
+
+public class Tets : MonoBehaviour
+{
+    MeshFilter meshFilter;
+    public Image img;
+    public Button btn;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        ////查看源码调试 过程
+        btn.onClick.AddListener(() =>
+        {
+            Debug.Log("点击响应");
+        });
+     
+
+
+        // 这一帧 设置color属性 =》  Graphic的基类 SetVerticesDirty =》添加 接口ICanvasElement 进入CanvasUpdateRegistry 的渲染 容器。 =》下一帧
+        img.color = Color.green;
+        //下一帧 Canvas.willRenderCanvases事件 =》进入CanvasUpdateRegistry的方法PerformUpdate =》   把  CanvasUpdateRegistry 的ICanvasElement渲染容器 按顺序调用并移除。 完成一次 渲染（如果 也添加进了 ICanvasElement布局容器 ，先整体把 布局调用 ，然后 在渲染） 
+
+        //btn.GetComponentInChildren<Text>().text = "545";
+
+        Debug.Log(222);
+    }
+    public void ClickTest()
+    {
+        Debug.Log("点击响应");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("img.depth" + img.depth);
+        //if (Input.anyKeyDown)
+        //{
+        //    RaycastHit hit;
+        //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    bool isHit = Physics.Raycast(ray, out hit);
+        //    if (isHit)
+        //    {
+        //        Debug.Log(hit.collider.gameObject.name);
+
+        //    }
+
+        //}
+
+
+
+        meshFilter = GetComponent<MeshFilter>();
+        VertexHelper vertexHelper = new VertexHelper();
+        Color color = Color.red;
+        vertexHelper.AddVert(new Vector2(0, 0), color, new Vector2(0, 0));
+        vertexHelper.AddVert(new Vector2(1, 1), color, new Vector2(1, 1));
+        vertexHelper.AddVert(new Vector2(0, 1), color, new Vector2(0, 1));
+        vertexHelper.AddVert(new Vector2(1, 0), color, new Vector2(1, 0));
+        //三角形状的 0 1 2 3 分别对应 上面添加的 坐标顺序。
+        vertexHelper.AddTriangle(0, 1, 2);
+        vertexHelper.AddTriangle(0, 3, 1);
+        vertexHelper.FillMesh(meshFilter.mesh);
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # UGUISourceAnalyze
+
+
+#配置项目
+
+1. 使用Unity的版本2018.4.30f1  
+2. Unity删除UI包 Window =>   Pakage Manager=> 移除ui相关的包
+3. Unity编辑器目录中 删除 掉 GUISystem 。路径 
+ 2018.4.30f1\Editor\Data\UnityExtensions\Unity\GUISystem
+4. 添加 从Github中的 https://github.com/Unity-Technologies/uGUI 找一个 2018.4.30f1 版本可兼容的 UGUI 源码  分支。 放入项目文件中。（删除掉vs项目的 配置文件 例如：lib，proptys 等。）
